4b116e016c065bceeaa0c46a7edc1e2442b3f119
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-05-16 02:20:24 +0800

    [jiefeng] add history to the route render method.

--- src/render.js
+++ src/render.js
@@ -1,13 +1,14 @@
 const React = require('react');
 const ReactRouter = require('react-router');
+const createBrowserHistory = require('history/createBrowserHistory');
 const {ReactRouter013, ReactRouter3} = require('./version');
 
 function render(reactRender, routes, el, callback) {
   if (ReactRouter013) {
-    ReactRouter.run([routes], callback);
+    ReactRouter.run([routes], ReactRouter.HistoryLocation, callback);
   } else if (ReactRouter3) {
     const Router = ReactRouter.Router;
-    reactRender(<Router routes={routes} />, el);
+    reactRender(<Router routes={routes} history={createBrowserHistory()}/>, el);
   }
 }
 
