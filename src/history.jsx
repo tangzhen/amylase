d3b4e1ef2fda17deb413e265ebfbe2c660226864
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-08-09 21:33:03 +0800

    [jifeng] add push, replace, goback method for the memoryRouter.

--- src/history.jsx
+++ src/history.jsx
@@ -6,42 +6,99 @@ const {ReactRouter013, ReactRouter3, ReactRouter4} = require('./version');
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component'
 }
-
+function getLocation(path, pushState) {
+  let search, pathname, state;
+  if (typeof path === 'string') {
+    const searchIndex = path.indexOf('?');
+    if (searchIndex !== -1) {
+      search = path.substr(searchIndex);
+      pathname = path.substr(0, searchIndex)
+    } else {
+      pathname = path;
+      search = ''
+    }
+    state = pushState;
+  } else {
+    if (path.search) {
+      const isSearchStartWith = path.search.charAt(0) !== '?';
+      search = isSearchStartWith ? '?' + path.search: path.search;
+    } else {
+      search = ''
+    }
+    pathname = !!path.pathname ? path.pathname : '';
+    state = !! path.state ? path.state: {}
+  }
+  return {pathname, state, search: search === '?' ? '' : search};
+}
+function transformQueryToSearch(query) {
+  return _.chain(query)
+    .map((value, key)=> `${key}=${value}`)
+    .join('&')
+    .value();
+}
 class MemoryRouter extends React.Component {
   constructor(props) {
     super(props);
     this.history = {
+      entries: [],
       location: {
-        pathname: _.get(props, `initialEntries[${props.initialIndex}]`) || '/'
+        pathname: _.get(props, `initialEntries[${props.initialIndex}]`) || '/',
+        search: '',
+        state: {}
+      },
+      action: '',
+      push: (path, pushState) => {
+        const { pathname, search, state } = getLocation(path, pushState);
+        this.history.location = {
+          pathname,
+          search,
+          state
+        };
+        this.history.action = 'PUSH';
+      },
+      replace: (path, pushState) => {
+        const { pathname, search, state } = getLocation(path, pushState);
+        this.history.location = {
+          pathname,
+          search,
+          state
+        };
+        this.history.action = 'REPLACE';
+      },
+      goBack: () => {
+        this.history.action = 'POP';
+        this.history.location.pathname = _.get(props, `initialEntries[${props.initialIndex -1}]`) || '/'
       }
     };
     this.router = () => {};
     if (ReactRouter013) {
-      this.router.transitionTo = (path) => {
-        this.history.location.pathname = path;
+      this.router.transitionTo = (path, params, query) => {
+        //TODO: map the params to path.
+        this.history.push({pathname: path, search: transformQueryToSearch(query)});
       };
-      this.router.replaceWith = (path) => {
-        this.history.location.pathname = path;
+      this.router.replaceWith = (path, params, query) => {
+        //TODO: map the params to path.
+        this.history.replace({pathname: path, search: transformQueryToSearch(query)});
       };
     } else {
-      this.router.push = (path) => {
-        this.history.location.pathname = path;
+      this.router.push = (path, state) => {
+        this.history.push(path, state);
       };
-      this.router.replace = (path) => {
-        this.history.location.pathname = path;
+      this.router.replace = (path, state) => {
+        this.history.replace(path, state);
       };
     }
-
     this.index = props.initialIndex;
 
     this.router.goBack = () => {
       this.history.location.pathname = _.get(this.props, `initialEntries[${this.index - 1}]`) || this.history.location.pathname;
+      this.history.goBack();
     };
   }
 
   static propTypes = {
     initialEntries: PropTypes.array,
-    initialIndex: PropTypes.number
+    initialIndex: PropTypes.number,
   };
 
   render() {
