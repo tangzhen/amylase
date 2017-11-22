81b44c464deba3909af2e1412f4829f1df6aa4e1
TangZhen <cloudztang@gmail.com>
2017-11-22 11:11:24 +0800

    [Zhen] fix typo.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const {ReactRouter4} = require('./version');
 
-export const Link = ReactRouter4 ? require('react-router-dome').Link : require('react-router').Link;
+export const Link = ReactRouter4 ? require('react-router-dom').Link : require('react-router').Link;
 export {createRoute, createRouteComponentWithConfig} from './createRoute';
 export render from './render';
 export {MemoryRouter, withRouter, push, replace, goBack} from './history';
